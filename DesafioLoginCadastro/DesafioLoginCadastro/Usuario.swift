4e6a5b7fd0c6e59bded99b50b523901a94bd5500
joorgeroberto <jorgercj@dcomp.ufs.br>
2020-09-15 21:01:29 -0300

    first commit

---
+++ DesafioLoginCadastro/DesafioLoginCadastro/Usuario.swift
@@ -0,0 +1,9 @@
+//
+//  Usuario.swift
+//  DesafioLoginCadastro
+//
+//  Created by Jorge on 14/09/20.
+//  Copyright © 2020 Jorge. All rights reserved.
+//
+
+import Foundation
---
+++ DesafioLoginCadastro/DesafioLoginCadastro/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  DesafioLoginCadastro
+//
+//  Created by Jorge on 14/09/20.
+//  Copyright © 2020 Jorge. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
